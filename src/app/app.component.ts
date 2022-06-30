379e6f8cc454837f7f7e804947d462d3225512f7
Adrien Merignac <adrien.merignac@gmail.com>
2022-06-30 16:55:08 +0200

    Le résultat du test LBO (non terminé)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,50 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'souschaine';
+  /* la chaîne de caractère */
+  maChaine = '';
+  /* la longueur souhaitée */
+  longueur = '';
+  /* les différents resultats en fonction de la chaîne de caractère et de la longueur */
+  resultats: number[] = [];
+  erreur = '';
+
+
+  calculerLesPlusGrandsNombres() {
+    console.log('1');
+    const maChaineEntier = parseInt(this.maChaine);
+    const maLongueurEntier = parseInt(this.longueur);
+    if(this.maChaine && this.longueur && !isNaN(maChaineEntier) && !isNaN(maLongueurEntier)) {
+
+      if (maLongueurEntier > maChaineEntier) {
+        this.erreur = 'Merci de saisir une longueur inférieur à la chaîne de caractères.';
+      }
+      else {
+        const itérations = maChaineEntier - maLongueurEntier;
+        for (let i = 0; i < itérations; i++) {
+          this.resultats = [...this.resultats, this.multiplierChaine(this.maChaine.slice(i, maLongueurEntier))]
+        }
+        console.log(this.resultats);
+        this.erreur = '';
+        // On procède au calcul et affiche le résultat de la plus grande multiplication
+        const plusGrandNombre = Math.max(...this.resultats);
+        console.log(plusGrandNombre);
+      }
+    }
+    else {
+      this.erreur = 'Merci de saisir des nombres entiers.';
+    }
+  }
+
+  multiplierChaine(value: string) {
+    return value.split('').reduce((acc, currentValue) => {
+      if(!acc) {
+        acc = parseInt(currentValue);
+        return acc;
+      }
+      acc *= parseInt(currentValue);
+      return acc;
+    }, 0)
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import {MatInputModule} from '@angular/material/input';
+import { FormsModule } from '@angular/forms';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NoopAnimationsModule,
+    MatInputModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
